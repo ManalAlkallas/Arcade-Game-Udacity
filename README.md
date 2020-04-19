055abbd75795ad9e17e7aa8e57775dd0384df144
ManalAlkallas <manalkallas@hotmail.com>
2020-04-19 02:55:23 +0200

    Final Project

--- README.md
+++ README.md
@@ -1,71 +1,24 @@
-﻿
-# Udacity Classic Arcade Game Clone
+﻿# Udacity Classic Arcade Game Clone
 
-## Summary :speech_balloon:
-Custom frogger Arcade is a variation of the classic Frogger arcade game developed by Konami and licensed for North American distribution by Sega-Gremlin and worldwide by Sega itself. It is regarded as a classic from the golden age of video arcade games, noted for its novel gameplay and theme. [wiki/Frogger](https://en.wikipedia.org/wiki/Frogger)
 
-#### Objectives of the game :running:
-The object of the game is simple, you must direct the hero character across the grey brick road, full of enemy bugs, towards the water without getting hit.
+ This game consists of a player character and enemy bugs. The goal of the game is to reach the water, without colliding into any of the enemies, and the player can move left, right, up and down with the keyboard arrow keys. The the killer bugs move in varying speeds across the screen and your manual dexterity and quick reflexes are needed to avoid the enemy bugs and to reach the water. Once the player collides with an enemy, the game is reset and the player moves back to the start square. Once the player reaches the water ten times with out losing three lives, the game is won. Run fast!
 
-Your score is based on your current score + (level x 2) for each level you pass.
+ ## Live Demo
 
-The enemy bugs run across the screen at random speeds in the x direction and random y locations given their constraints which is between the grey bricks.
+https://ManalAlkallas.github.io/Arcade-Game-Udacity/
 
-The number of enemy bugs will increase by 1 until level 6 where an increase of 1 bug will be added to the total enemies each time you complete the level.
+ ## Languages / Tools used
 
-If you run into an enemy bug, you will reset, your level decreases by 1, your score decreases by 1 and the enemy you run into will disappear.
+* HTML & CSS
+* HTML5 Canvas
+* JavaScript
 
-![frogger image](/images/froggerwill.png)
+## Getting started
+Open the index.html file in any modern browser, click inside the game area, and enjoy playing the game.
 
-#### How to play and the controls :video_game:
-Users control the hero character by using the main keyboard keys: [  ←   →   ↑   ↓  ] arrow keys on their keyboard. There is no press down and hold option, you must press each key to move in that specific direction.
+ 
 
-## Demo the game
-* Play Zeh Demo: [Demo Me](https://iamwill123.github.io/Frogger-Arcade-Game/game.html "Play game demo")
+ 
 
-## Github pages website
-* Github pages website: [Frogger Arcade Game](https://iamwill123.github.io/Frogger-Arcade-Game/ "Github Pages Website")
 
-## Modifications of your own :raised_hands:
 
-1. Download the GitHub zip file or clone the repository onto your local workstation:
-    * zip file: [https://github.com/iamwill123/Frogger-Arcade-Game/archive/master.zip](https://github.com/iamwill123/Frogger-Arcade-Game/archive/master.zip "Zip file")
-    * git clone: [https://github.com/iamwill123/Frogger-Arcade-Game.git](https://github.com/iamwill123/Frogger-Arcade-Game.git "clone repository")
-
-2. To check out the game, navigate to the index.html file in your application's directory and double click to open in your browser.
-
-## Tools & Techniques involved
-1. Object-Oriented JavaScript
-    - Scopes
-    - Closures
-    - The 'this' Keyword
-    - Prototype Chains
-    - Object Decorator Pattern
-    - Functional Classes
-    - Prototypal Classes
-    - Superclass and Subclass
-    - Pseudoclassical Subclasses
-2. HTML5 Canvas API
-    - HTML5 Canvas Basics
-    - From Pixels to Animation
-
-## List of Resources
-[http://www.w3schools.com/Tags/ref_canvas.asp](http://www.w3schools.com/Tags/ref_canvas.asp "HTML5 Canvas Reference")
-
-[https://github.com/udacity/frontend-nanodegree-arcade-game](https://github.com/udacity/frontend-nanodegree-arcade-game "Udacity Frontend arcade game")
-
-===============================
-
-##The Udacity Project Requirements
-
-### How do I complete this project?
-
-#### Students should use this rubric for self-checking their submission:
-[https://www.udacity.com/course/viewer#!/c-ud015/l-3072058665/m-3072588797/](https://www.udacity.com/course/viewer#!/c-ud015/l-3072058665/m-3072588797/ "Frogger Arcade Game Project Requirements")
-
-##frontend-nanodegree-arcade-game
-===============================
-
-Students should use this [rubric](https://review.udacity.com/#!/projects/2696458597/rubric) for self-checking their submission. Make sure the functions you write are **object-oriented** - either class functions (like Player and Enemy) or class prototype functions such as Enemy.prototype.checkCollisions, and that the keyword 'this' is used appropriately within your class and class prototype functions to refer to the object the function is called upon. Also be sure that the **readme.md** file is updated with your instructions on both how to 1. Run and 2. Play your arcade game.
-
-For detailed instructions on how to get started, check out this [guide](https://docs.google.com/document/d/1v01aScPjSWCCWQLIpFqvg3-vXLH2e8_SZQKC8jNO0Dc/pub?embedded=true).
--- js/app.js
+++ js/app.js
@@ -1,33 +1,30 @@
-// Superclass
-var Character = function(sprite, x, y, name) {
-    this.sprite = sprite;
+
+var Bug = function(ghost, x, y, name) {
+    this.ghost = ghost;
     this.x = x;
     this.y = y;
     this.name = name;
 };
 
-Character.prototype.render = function() {
-    // calls the context from canvas to draw the sprite image
-    // and to position the image
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+Bug.prototype.render = function() {
+    // calls the context from canvas to draw the ghost image
+    ctx.drawImage(Resources.get(this.ghost), this.x, this.y);
     ctx.font = "15px Roboto";
     ctx.textAlign = "center";
-    // gave characters a unique name
+    // gave the Bug a unique name
     ctx.fillText(this.name, this.x + 50, this.y + 155);
 };
 
-//Subclasses
-
 // Enemies our player must avoid
-var Enemy = function(sprite, x, y, name, movement) {
-    // Calling the character constructor so we can pass
+var Enemy = function(ghost, x, y, name, movement) {
+    // Calling the Bug constructor so we can pass
     // in those arguments so we don't have to re-write the code
-    Character.call(this, sprite, x, y, name);
+    Bug.call(this, ghost, x, y, name);
     // enemy has it's own unique movement
     this.movement = movement;
 };
-// Enemy prototype to inherit Character.prototype's methods
-Enemy.prototype = Object.create(Character.prototype);
+// Enemy prototype to inherit Bug.prototype's methods
+Enemy.prototype = Object.create(Bug.prototype);
 Enemy.prototype.constructor = Enemy;
 
 // Update the enemy's position, required method for game
@@ -47,23 +44,18 @@ Enemy.prototype.update = function(dt) {
         this.x = -2;
         this.y = Math.random() * 184 + 50;
     }
-    // Method to check if enemy and player collided or not
+    // Check if the enemy and the player collided 
     checkCollision();
 };
 
-// Player we control
-var Player = function(sprite, x, y, name, movement) {
-    Character.call(this, sprite, x, y, name);
+var Player = function(ghost, x, y, name, movement) {
+    Bug.call(this, ghost, x, y, name);
     // This sets the player to move at 50px
     this.movement = 50;
 };
 
-Player.prototype = Object.create(Character.prototype);
+Player.prototype = Object.create(Bug.prototype);
 Player.prototype.constructor = Player;
-
-// Use this update method later
-// Player.prototype.update = function(dt) {
-// };
 
 //Input handler for player
 Player.prototype.handleInput = function(e) {
@@ -75,10 +67,10 @@ Player.prototype.handleInput = function(e) {
         this.x += this.movement;
     }
     else if (e === 'up'){
-        // This contraint makes sure you reset each time you reach the water.
+        //  Makes sure to reset each time after reaching the water.
         if (this.y < 40){
 
-        this.reset(); // resets player
+        this.reset(); 
 
         level++; // adds 1 to the current level
         score += level * 2; // adds to the score
@@ -96,46 +88,46 @@ Player.prototype.handleInput = function(e) {
         this.y += this.movement;
     }
 };
-// Player reset back to original position
+// Player returns back to original position
 Player.prototype.reset = function() {
     this.x = 200;
     this.y = 400;
 };
 
-//Level increases
+// Increase then level
 var increaseLevel = function(level) {
     allEnemies.length = 0;
-    // list of names for the enemy
-    var name = ['billy', 'Joel', 'Snickers', 'Homer', 'The terminator', 'ROBO-COP', 'Ice man', 'Finn', 'Jake', 'Final Boss'];
+    // A list of enemy's names
+    var name = ['Venus', 'Earth', 'Mars', 'Jupiter', 'The Saturn', 'Uranus', 'Neptune', 'Moon', 'Sun'];
     // new set of enemies for loop
     for (var i = 0; i < level; i++) {
         var enemy = new Enemy('images/enemy-bug.png', 0, Math.random() * 184 + 50, name[i], Math.random() * 256);
         allEnemies.push(enemy);
     }
-    // Add 1 more enemy after level 5
-    if (level > 5) {
+    // Add 1 more enemy after level 6
+    if (level > 6) {
         var enemy = new Enemy('images/enemy-bug.png', 0, Math.random() * 184 + 50, name[i], Math.random() * 256);
         allEnemies.push(enemy);
     }
 };
-// decreasing enemy, level and score
+// increasing the numbers of enemy, level and scores
 var decreaseLevel = function() {
     if (allEnemies.length >= 1) {
         allEnemies.pop(enemy);
-        level--; // adds to the current level
-        score--; // adds to score, and adds more enemies
+        level--; // update the level
+        score--; // add score, and more enemies
     }
 }
 
-// Checking for collision
+// Collision checking
 var checkCollision = function() {
-    //If the player reachs enemy proximity by 40px in all directions, execute the following
+    //If the player reaches enemy proximity by 40px in all directions, execute the following
     for (var i = 0; i < allEnemies.length; i++) {
         if (Math.abs(player.x - allEnemies[i].x) <= 40) {
             if (Math.abs(player.y - allEnemies[i].y) <= 40) { //Math.abs(-x) = x
                 player.reset();
                 decreaseLevel(); // decrease level
-                displayStats(); // update stats when levels decrease
+                displayStats(); // update stats when levels drop
                 console.log('current score: ' + score); //check to see that the stats work
                 console.log('current level: ' + level);
             }
@@ -143,7 +135,7 @@ var checkCollision = function() {
     }
 };
 
-// Display stats
+// Display statistics
 var displayStats = function() {
     document.getElementById('currentStats').innerHTML = 'Level: ' + level.toString() + " | " + 'Score: ' + score.toString();
     document.getElementById('numberOfEnemies').innerHTML = 'Enemies: ' + allEnemies.length.toString();
@@ -152,18 +144,18 @@ var displayStats = function() {
 // Enemies storage place
 var allEnemies = [];
 
-// Instantiate a new player
-var player = new Player('images/char-boy.png', 200, 400, 'Will Yuan');
+// Create a player, position and name
+var player = new Player('images/char-pink-girl.png', 200, 400, 'Manal');
 
-// Start score at zero
+// Adding one enemy in the beginning
+var enemy = new Enemy('images/enemy-bug.png', -2, Math.random() * 184 + 50, 'Mercury', Math.random() * 256);
+allEnemies.push(enemy);
+
+// Start score
 var score = 0;
 
 // Start level at 1
 var level = 1;
-
-// Instantiate one enemy in the beginning
-var enemy = new Enemy('images/enemy-bug.png', -2, Math.random() * 184 + 50, 'Bob', Math.random() * 256);
-allEnemies.push(enemy);
 
 //Call display stats globally
 displayStats();
